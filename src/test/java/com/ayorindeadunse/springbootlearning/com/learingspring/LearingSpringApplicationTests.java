1718dc0d2f6193eca685f166a752b8b1036a8191
ayorindeadunse <adunseayorinde@gmail.com>
2022-04-29 01:14:10 +0100

    bootstrapping spring learning course, learing-spring

---
+++ src/test/java/com/ayorindeadunse/springbootlearning/com/learingspring/LearingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayorindeadunse.springbootlearning.com.learingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
