51c0bc5d97e7cfb10e649c8abacab6d5cb00f6be
ayorindeadunse <adunseayorinde@gmail.com>
2022-04-30 19:55:12 +0100

    updated RoomRepository

--- src/main/java/com/ayorindeadunse/springbootlearning/com/learningspring/util/AppStartupEvent.java
+++ src/main/java/com/ayorindeadunse/springbootlearning/com/learningspring/util/AppStartupEvent.java
@@ -1,4 +1,23 @@
 package com.ayorindeadunse.springbootlearning.com.learningspring.util;
 
-public class AppStartupEvent {
+import com.ayorindeadunse.springbootlearning.com.learningspring.data.Room;
+import com.ayorindeadunse.springbootlearning.com.learningspring.data.RoomRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final RoomRepository roomRepository;
+
+    public AppStartupEvent(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+    Iterable<Room> rooms = roomRepository.findAll();
+    rooms.forEach(System.out::println);
+    }
 }
