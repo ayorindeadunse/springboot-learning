766560697e0b5582c8b713e3c556dc9f1c3def37
ayorindeadunse <adunseayorinde@gmail.com>
2022-04-30 19:39:37 +0100

    Added Room entity

---
+++ src/main/java/com/ayorindeadunse/springbootlearning/com/learningspring/data/Room.java
@@ -0,0 +1,18 @@
+package com.ayorindeadunse.springbootlearning.com.learningspring.data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ROOM")
+public class Room {
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+@Column(name = "ROOM_ID")
+private long id;
+@Column(name = "NAME")
+private String name;
+@Column(name = "ROOM_NUMBER")
+private String roomNumber;
+@Column(name = "BED_INFO")
+private String bedInfo;
+}
