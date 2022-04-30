186f868cd2f9789c52db9ab58b51d74b3933bc9a
ayorindeadunse <adunseayorinde@gmail.com>
2022-04-30 19:45:08 +0100

    Added Room Repository

---
+++ src/main/java/com/ayorindeadunse/springbootlearning/com/learningspring/data/RoomRepository.java
@@ -0,0 +1,6 @@
+package com.ayorindeadunse.springbootlearning.com.learningspring.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoomRepository extends CrudRepository<Room,Long> {
+}
